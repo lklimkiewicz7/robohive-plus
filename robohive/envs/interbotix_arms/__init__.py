89cf47daf1fb44451223a55f8494e2f749e60f0f
lklimkiewicz7 <lklimkiewicz@user.com>
2023-10-26 13:32:38 +0200

    Add missing joints

--- robohive/envs/interbotix_arms/__init__.py
+++ robohive/envs/interbotix_arms/__init__.py
@@ -11,7 +11,7 @@ print("RoboHive:> Registering Interbotix Arms Envs")
 register(
     id='InterbotixVX300PushFixed-v0',
     entry_point='robohive.envs.arms.push_base_v0:PushBaseV0',
-    max_episode_steps=50, #50steps*40Skip*2ms = 4s
+    max_episode_steps=500, #50steps*40Skip*2ms = 4s
     kwargs={
         'model_path': curr_dir+'/vx300/vx300_ycb_v0.xml',
         'config_path': curr_dir+'/vx300/vx300_ycb_v0.config',
