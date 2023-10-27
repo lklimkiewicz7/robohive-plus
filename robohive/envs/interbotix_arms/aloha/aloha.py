2a1c079db9375a5ce8ac052c1454a063523dcd84
lklimkiewicz7 <lklimkiewicz@user.com>
2023-10-27 19:31:04 +0200

    Add second arm

--- robohive/envs/interbotix_arms/aloha/aloha.py
+++ robohive/envs/interbotix_arms/aloha/aloha.py
@@ -7,10 +7,10 @@ from robohive.envs import env_base
 class AlohaEnv(env_base.MujocoEnv):
 
     DEFAULT_OBS_KEYS = [
-        'qp_robot', 'qp_object', 'qv_robot', 'qv_object', 'object_err', 'target_err'
+        'qp_robot', 'qv_robot'
     ]
     DEFAULT_PROPRIO_KEYS = [
-        'qp_robot', 'qp_object', 'qv_robot', 'qv_object'
+        'qp_robot', 'qv_robot',
     ]
     DEFAULT_VISUAL_KEYS = [
         'rgb:top_cam:160x256:2d',
@@ -18,29 +18,11 @@ class AlohaEnv(env_base.MujocoEnv):
         'rgb:vx300_arm0_gripper_cam:160x256:2d',
         'rgb:vx300_arm1_gripper_cam:160x256:2d',
     ]
-    DEFAULT_RWD_KEYS_AND_WEIGHTS = {
-        "object_dist": -1.0,
-        "target_dist": -1.0,
-        "bonus": 4.0,
-        "penalty": -50,
-    }
+    DEFAULT_RWD_KEYS_AND_WEIGHTS = {}
 
     def __init__(self, model_path, obsd_model_path=None, seed=None, **kwargs):
-
-        # EzPickle.__init__(**locals()) is capturing the input dictionary of the init method of this class.
-        # In order to successfully capture all arguments we need to call gym.utils.EzPickle.__init__(**locals())
-        # at the leaf level, when we do inheritance like we do here.
-        # kwargs is needed at the top level to account for injection of __class__ keyword.
-        # Also see: https://github.com/openai/gym/pull/1497
         gym.utils.EzPickle.__init__(self, model_path, obsd_model_path, seed, **kwargs)
-
-        # This two step construction is required for pickling to work correctly. All arguments to all __init__
-        # calls must be pickle friendly. Things like sim / sim_obsd are NOT pickle friendly. Therefore we
-        # first construct the inheritance chain, which is just __init__ calls all the way down, with env_base
-        # creating the sim / sim_obsd instances. Next we run through "setup"  which relies on sim / sim_obsd
-        # created in __init__ to complete the setup.
         super().__init__(model_path=model_path, obsd_model_path=obsd_model_path, seed=seed)
-
         self._setup(**kwargs)
 
 
@@ -76,28 +58,15 @@ class AlohaEnv(env_base.MujocoEnv):
         obs_dict['time'] = np.array([self.sim.data.time])
         obs_dict['qp_robot'] = sim.data.qpos[:self.robot_ndof].copy()
         obs_dict['qv_robot'] = sim.data.qvel[:self.robot_ndof].copy()
-        obs_dict['qp_object'] = sim.data.qpos[self.robot_ndof:].copy()
-        obs_dict['qv_object'] = sim.data.qvel[self.robot_ndof:].copy()
-        obs_dict['object_err'] = sim.data.site_xpos[self.object_sid]-sim.data.site_xpos[self.grasp_sid]
-        obs_dict['target_err'] = sim.data.site_xpos[self.target_sid]-sim.data.site_xpos[self.object_sid]
         return obs_dict
 
 
     def get_reward_dict(self, obs_dict):
-        object_dist = np.linalg.norm(obs_dict['object_err'], axis=-1)
-        target_dist = np.linalg.norm(obs_dict['target_err'], axis=-1)
-        far_th = 1.25
-
         rwd_dict = collections.OrderedDict((
-            # Optional Keys
-            ('object_dist',   object_dist),
-            ('target_dist',   target_dist),
-            ('bonus',   (object_dist<.1) + (target_dist<.1) + (target_dist<.05)),
-            ('penalty', (object_dist>far_th)),
             # Must keys
-            ('sparse',  -1.0*target_dist),
-            ('solved',  target_dist<.050),
-            ('done',    object_dist > far_th),
+            ('sparse',  0),
+            ('solved',  False),
+            ('done',    False),
         ))
         rwd_dict['dense'] = np.sum([wt*rwd_dict[key] for key, wt in self.rwd_keys_wt.items()], axis=0)
         return rwd_dict
