776ebff5272de55695c3795207c186cac69a152f
lklimkiewicz7 <lklimkiewicz@user.com>
2023-12-04 14:38:16 +0100

    Add script for replaying trajectory from scalingup

--- robohive/envs/ur5/__init__.py
+++ robohive/envs/ur5/__init__.py
@@ -9,7 +9,7 @@ from robohive.envs.arms.reach_base_v0 import ReachBaseV0
 curr_dir = os.path.dirname(os.path.abspath(__file__))
 print("RoboHive:> Registering UR5 Envs")
 
-x=0
+
 register(
     id='Ur5ReachFixed-v0',
     entry_point='robohive.envs.arms.reach_base_v0:ReachBaseV0',
@@ -19,6 +19,19 @@ register(
         'config_path': curr_dir+'/assets/ur5_reach.config',
         'robot_site_name': "end_effector",
         'target_site_name': "target",
-        'target_xyz_range': {'high':[0.2+x, 0.3+x, 1.2+x], 'low':[0.2+x, 0.3+x, 1.2+x]}
+        'target_xyz_range': {'high':[0.2, 0.3, 1.2], 'low':[0.2, 0.3, 1.2]},
+        'normalize_act': False,
     }
+)
+
+register_env_variant(
+    env_id='Ur5ReachFixed-v0',
+    variant_id='Ur5ReachFixedCam-v0',
+    variants={
+            'visual_keys':[
+                "rgb:top_down:224x224:2d",
+                "rgb:front:224x224:2d",
+            ],
+        },
+    silent=True
 )
