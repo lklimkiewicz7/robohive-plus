0dfb74bcb7e076325bbbb29c7be3ea358b321621
lklimkiewicz7 <lklimkiewicz@user.com>
2023-12-06 12:28:28 +0100

    Update few things to improve ur5 sim compatibility

--- robohive/envs/ur5/__init__.py
+++ robohive/envs/ur5/__init__.py
@@ -3,7 +3,7 @@ import os
 from gym.envs.registration import register
 
 from robohive.envs.env_variants import register_env_variant
-from robohive.envs.arms.reach_base_v0 import ReachBaseV0
+from robohive.envs.ur5.ur5_env import Ur5Env
 
 
 curr_dir = os.path.dirname(os.path.abspath(__file__))
@@ -12,14 +12,10 @@ print("RoboHive:> Registering UR5 Envs")
 
 register(
     id='Ur5ReachFixed-v0',
-    entry_point='robohive.envs.arms.reach_base_v0:ReachBaseV0',
-    max_episode_steps=1000,
+    entry_point='robohive.envs.ur5.ur5_env:Ur5Env',
     kwargs={
         'model_path': curr_dir+'/assets/ur5_reach.xml',
         'config_path': curr_dir+'/assets/ur5_reach.config',
-        'robot_site_name': "end_effector",
-        'target_site_name': "target",
-        'target_xyz_range': {'high':[0.2, 0.3, 1.2], 'low':[0.2, 0.3, 1.2]},
         'normalize_act': False,
     }
 )
--- robohive/envs/ur5/ur5_env.py
+++ robohive/envs/ur5/ur5_env.py
@@ -1,60 +1,45 @@
 import collections
+import random
+from math import pi
+
 import gym
 import numpy as np
 
 from robohive.envs import env_base
-from robohive.robot.robot_interbotix_arm import InterbotixArmRobot
 
-class InterbotixSimpleEnv(env_base.MujocoEnv):
+class Ur5Env(env_base.MujocoEnv):
     
-    def __init__(self, model_path, n_arms, arm_dof, obsd_model_path=None, seed=None, **kwargs):
+    INITIAL_POS = [-2.30, -2.34, 1.85, -1.06, -1.57, -0.73, 0.054, 0.054]
+    DEFAULT_OBS_KEYS = ['position', 'velocity']
+    DEFAULT_PROPRIO_KEYS = ['position']
+    
+    def __init__(self, model_path, obsd_model_path=None, seed=None, **kwargs):
         gym.utils.EzPickle.__init__(self, model_path, obsd_model_path, seed, **kwargs)
         super().__init__(model_path=model_path, obsd_model_path=obsd_model_path, seed=seed)
-        self.n_arms = n_arms
-        self.arm_dof = arm_dof
-        self._setup(robot_dof=arm_dof, robot_n_arms=n_arms, **kwargs)
+        self._setup(**kwargs)
 
     def _setup(self,
-               robot_dof,
-               robot_n_arms,
                frame_skip=40,
                reward_mode="dense",
+               obs_keys=DEFAULT_OBS_KEYS,
+               proprio_keys=DEFAULT_PROPRIO_KEYS,
                **kwargs,
-        ):
-        obs_keys = self._generate_obs_keys()
-        proprio_keys = self._generate_proprio_keys()
-        
+        ):        
         super()._setup(obs_keys=obs_keys,
                        proprio_keys=proprio_keys,
                        weighted_reward_keys={},
                        reward_mode=reward_mode,
                        frame_skip=frame_skip,
-                       robot_class=InterbotixArmRobot,
-                       robot_dof=robot_dof,
-                       robot_n_arms=robot_n_arms,
                        **kwargs)
-
-    def _generate_obs_keys(self):
-        return self._generate_proprio_keys()
         
-    def _generate_proprio_keys(self):
-        prioprio_keys = []
-        for i in range(self.n_arms):
-            prioprio_keys.append(f'qp_arm{i}')
-            prioprio_keys.append(f'qv_arm{i}')
-        return prioprio_keys
+        self.init_qpos = self.INITIAL_POS
+        self.reset()
         
     def get_obs_dict(self, sim):
         obs_dict = {}
         obs_dict['time'] = np.array([self.sim.data.time])
-        
-        for i in range(self.n_arms):
-            arm_range = slice(i*self._arm_length, (i+1)*self._arm_length)
-            arm_qpos = sim.data.qpos[arm_range].copy()
-            arm_qvel = sim.data.qvel[arm_range].copy()
-            obs_dict[f'qp_arm{i}'] = arm_qpos
-            obs_dict[f'qv_arm{i}'] = arm_qvel
-            
+        obs_dict['position'] = sim.data.qpos.copy()[:7]
+        obs_dict['velocity'] = sim.data.qvel.copy()[:7]
         return obs_dict
 
 
@@ -68,9 +53,26 @@ class InterbotixSimpleEnv(env_base.MujocoEnv):
         return rwd_dict
 
     def reset(self):
-        obs = super().reset()
+        object_pos = self._random_position()
+        object_rot = self._random_rotation()
+        object_data = object_pos + [1] + object_rot
+        obs = super().reset(reset_qpos=self.INITIAL_POS + object_data)
+        
+        self.sim.data.ctrl[:] = self.INITIAL_POS[:-1]
+        self.sim.advance(substeps=500, render=False)
+        
         return obs
-
-    @property
-    def _arm_length(self):
-        return self.arm_dof + 3
+    
+    def _random_position(self):
+        return [
+            random.uniform(0.3, 0.7),
+            random.uniform(-0.45, 0.45),
+            0.1
+        ]
+        
+    def _random_rotation(self):
+        return [
+            random.uniform(-pi, pi),
+            random.uniform(-pi, pi),
+            random.uniform(-pi, pi),
+        ]
